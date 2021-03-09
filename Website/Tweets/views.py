6ffd8ee7dcaccdc715eab4b485581ff74bbc962e
unknown <jonAhmeti>
2021-03-09 22:49:46 +0100

    Finally one templates/Home/index up

--- Website/Tweets/views.py
+++ Website/Tweets/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your templates here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world.\nYou're at the Tweets index.")
+
+    return render(request, 'Home/index.html')
