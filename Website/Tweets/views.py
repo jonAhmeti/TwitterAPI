23a51bb407858fce818261384c77b8e53302219f
unknown <jonAhmeti>
2021-03-11 23:50:25 +0100

    idk what i did

--- Website/Tweets/views.py
+++ Website/Tweets/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+import Locations
 
 # Create your templates here.
 from django.http import HttpResponse
 
 
 def index(request):
-
+    for x in Locations:
+        print(x)
     return render(request, 'Home/index.html')
