63d371add7f31255158e9eea716a39141cc4d9ff
unknown <jonAhmeti>
2021-03-16 18:13:42 +0100

    Inputs page designed

--- Website/Tweets/views.py
+++ Website/Tweets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .Locations import Locations
+from .ISO639Lang import LangISO
 
 
 def index(request):
@@ -9,10 +10,18 @@ def index(request):
         for y in x.value:
             if y.name == 'full_name':
                 countries.append((y.value, x.name))
-
     countries.sort()
 
-    return render(request, 'Home/index.html', {'context_countries': countries})
+    languages = []
+    for lang in LangISO:
+        language_names = lang.value[1].split(',')
+        if len(language_names) > 1:
+            languages.append((lang.value[0], language_names[0]))
+        else:
+            languages.append(lang.value)
+    languages.sort(key=lambda x:x[1])
+
+    return render(request, 'Home/index.html', {'context_countries': countries, 'context_lang': languages})
 
 
 def cities(request):
